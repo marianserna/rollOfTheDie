747b0547c48887723163b5a56ec5d412977229c7
Marian Serna <marianhalliday@gmail.com>
2017-09-20 19:04:30 -0400

    8. permutations_total

---
+++ permutations_total.rb
@@ -0,0 +1,16 @@
+# Modify this program to also display the total of each permutation.
+#
+# Your output should look like so:
+#
+# $ ruby permutation_totals.rb
+# Dice Roll: 1, 1  Total: 2
+# Dice Roll: 1, 2  Total: 3
+# Dice Roll: 1, 3  Total: 4
+# Dice Roll: 1, 4  Total: 5
+
+dice1 = (1..6).to_a
+permutations = dice1.repeated_permutation(2).to_a
+
+permutations.each do |permutation|
+  puts "Dice Roll: #{permutation[0].to_i}, #{permutation[1].to_i} Total: #{permutation[0].to_i + permutation[1].to_i} "
+end
